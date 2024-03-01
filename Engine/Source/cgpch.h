efc09d4cfbe61f822270396d382aa10e78b90f6d
Caleb <cjmeepers@gmail.com>
2024-03-01 20:27:34 +0800

    more setup files

---
+++ Engine/Source/cgpch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <cstdint>
+
+#include "Core/Base.h"
+#include "Core/Log.h"
